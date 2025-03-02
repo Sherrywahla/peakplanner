dcf451ccf091b9976ff2709c9f51d87b6bb4b428
Sherrywahla <shaheryarwahla8@gmail.com>
2025-03-02 21:49:40 +0300

    admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ("username", "email", "is_active", "is_staff", "date_joined")
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("username", "email")
+    ordering = ("-date_joined",)
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- reminders/admin.py
+++ reminders/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Reminder
 
-# Register your models here.
+class ReminderAdmin(admin.ModelAdmin):
+    list_display = ("title", "user", "remind_at")
+    list_filter = ("remind_at",)
+    search_fields = ("title", "user__username")
+    ordering = ("-remind_at",)
+
+admin.site.register(Reminder, ReminderAdmin)
